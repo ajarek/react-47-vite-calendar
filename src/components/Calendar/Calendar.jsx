ac107d5190fa0cf7915275c384d3c7b7db19e206
Ajarek <ajarek@poczta.onet.pl>
2022-12-22 01:16:44 +0100

    added logic

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,15 +1,84 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import './Calendar.css'
 const Calendar = () => {
-  const [days,setDays]=useState([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31])
+ 
+  const today = new Date()
+  const [x, setX] = useState(2022)
+  const [y, setY] = useState(11)
+  const [z, setZ] = useState(today.getUTCDate())
+  const date = new Date(x, y, z)
+  const monthActual = date.getMonth() + 1
+  const yearActual = date.getFullYear()
+  const firstDayOfMonth = date.getDay() <= 0 ? 6 : date.getDay()
+
+  const arrData = new Array(firstDayOfMonth).fill('')
+  const [daysInWeek, setDaysInWeek] = useState(['Mon','Tue','Wed','Thu','Fri','Sat','Sun',])
+  const [days, setDays] = useState([])
+  const options = {month: 'long',}
+  
+  function getDaysInMonth(year, month) {
+    return new Date(year, month, 0).getDate()
+  }
+
+  function getDayInArray() {
+    days.length = 0
+    for (let i = 0; i < getDaysInMonth(yearActual, monthActual); i++) {
+      days.push(i + 1)
+    }
+  }
+  getDayInArray()
+
+  useEffect(() => {
+    const items = [...document.querySelectorAll(".day")];
+    items.map((item) => {
+       if(item.innerHTML==today.getDate()&&date.toLocaleDateString('en-US', options)===today.toLocaleDateString('en-US', options)&&x==today.getFullYear()) {
+       item.classList.add('active')
+       
+       }else{item.classList.remove('active')}
+      
+    });
+  
+ 
+},[x,y])
+
   return (
-    <div className='days-wrapper'>
-      {days.map(day=>{
-        return(
-          <div className="day">{day}</div>
-        )
-      })}
-    </div>
+    <>
+      <div className='calendar-header'>
+        <div className="header-wrapper">
+        <button onClick={()=>setY(y-1)}>◁</button>
+        <h4>{date.toLocaleDateString('en-US', options)}</h4>
+        <button onClick={()=>setY(y+1)}>▷</button>
+        </div>
+        <div className="header-wrapper">
+        <button onClick={()=>setX(x-1)}>◁</button>
+        <h4>{yearActual}</h4>
+        <button onClick={()=>setX(x+1)}>▷</button>
+        </div>
+      </div>
+      <div className='days-wrapper'  >
+        {daysInWeek.map((day) => {
+          return (
+            <div
+              key={day}
+              className='day'
+            >
+              {day}
+            </div>
+          )
+        })}
+        {arrData.concat(days).map((dey, index) => {
+          return (
+            <div
+              className={index==5||index==12||index==19||index==26||index==33||
+                index==6||index==13||index==20||index==27||index==34?'day red':'day'}
+              key={index}
+            >
+              {dey}
+            </div>
+          )
+        })}
+      </div>
+    </>
   )
 }
 
